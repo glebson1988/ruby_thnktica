bd896bae5a4092c23e390f267d65cd659a28ce96
Gleb Grishakov <glebson1988@yandex.ru>
2019-04-18 10:24:12 +0300

    refactoring task02

--- 01_ruby_basics/02_Ruby_basics_Part2/task02.rb
+++ 01_ruby_basics/02_Ruby_basics_Part2/task02.rb
@@ -1,6 +1,6 @@
 puts "Программа 'Заполнение массива от 10 до 100 с шагом 5'."
 puts '=' * 80
 
-arr = []
-(10..100).each { |num| arr << num if num % 5 == 0 }
+
+arr = (10..100).each.with_object([]) { |num, ar| ar << num if num % 5 == 0 }
 puts arr
