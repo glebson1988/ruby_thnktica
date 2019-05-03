ab922f17fa2d219984aa6f0dcd18ac4d15554792
Gleb Grishakov <glebson1988@yandex.ru>
2019-05-03 12:24:09 +0300

    Eight homework

---
+++ 01_ruby_basics/08_Ruby_idioms/station.rb
@@ -0,0 +1,50 @@
+class Station
+  include InstanceCounter
+  include Validation
+
+  EMPTY_NAME_ERROR = 'Название не может быть пустым.'.freeze
+  LONG_NAME_ERROR = 'Название слишком длинное.'.freeze
+
+  attr_reader :trains, :name
+
+  @@all_stations = []
+
+  def self.all
+    @@all_stations
+  end
+
+  def initialize(name)
+    register_instance
+    @name = name
+    validate!
+    @trains = []
+    @@all_stations << self
+  end
+
+  def arrive(train)
+    trains << train
+  end
+
+  def train_type(type)
+    trains.select { |train| train.type == type }
+  end
+
+  def depart(train)
+    trains.delete(train)
+  end
+
+  def to_s
+    name
+  end
+
+  def each_train
+    trains.each { |train| yield(train) }
+  end
+
+  protected
+
+  def validate!
+    raise EMPTY_NAME_ERROR if name == ''
+    raise LONG_NAME_ERROR if name.length > 10
+  end
+end
